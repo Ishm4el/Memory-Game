3d7f000f36cf89a0cc421c1e6ac4f27236c3cbd9
Ishii <ismaelv@protonmail.com>
2024-02-28 16:12:11 -0800

    Fetched the sprites from the pokeAPI and stored them inside the cardData

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,56 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
-const cardData = [{id: 0, selected: false}, {id: 1, selected: false}, {id: 2, selected: false}];
+const cardData = [
+  { id: 1, selected: false },
+  { id: 2, selected: false },
+  { id: 3, selected: false },
+];
 
 function shuffle(array) {
-  let currentIndex = array.length, randomIndex;
+  let currentIndex = array.length,
+    randomIndex;
 
   while (currentIndex > 0) {
     randomIndex = Math.floor(Math.random() * currentIndex--);
-    [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
   }
 }
 
-function Cards({handler}) {
+function Cards({ handler }) {
   shuffle(cardData);
-  return(cardData.map((value, index) => <p key={value.id} className="card-p" onClick={() =>  handler(value.selected, index)}>{value.id} {value.selected}</p>))
+
+  useEffect(() => {
+    Promise.all(
+      cardData.map((value) =>
+        fetch(`https://pokeapi.co/api/v2/pokemon-form/${value.id}/`).then(
+          (response) => response.json()
+        )
+      )
+    )
+      .then((responses) => {
+        const responseData = responses.map(
+          (response) => response.sprites.front_default
+        );
+        console.log("Fetched data:", responseData);
+        cardData.forEach((entry, index) => (entry.image = responseData[index]));
+        console.log(cardData);
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  });
+
+  return cardData.map((value, index) => (
+    <p
+      key={value.id}
+      className="card-p"
+      onClick={() => handler(value.selected, index)}
+    >
+      {value.id} {value.selected}
+    </p>
+  ));
 }
 
 function App() {
@@ -24,10 +60,9 @@ function App() {
     if (previouslySelected === false) {
       cardData[index].selected = true;
       setScore(score + 1);
-      console.log('here!');
-    }
-    else if (previouslySelected === true) {
-      cardData.forEach(value => value.selected = false);
+      console.log("here!");
+    } else if (previouslySelected === true) {
+      cardData.forEach((value) => (value.selected = false));
       setScore(0);
     }
   }
@@ -35,8 +70,7 @@ function App() {
   return (
     <>
       <Cards handler={handleScore} />
-      Hello
-      Score: {score}
+      Hello Score: {score}
       cardData: {JSON.stringify(cardData)}
     </>
   );
